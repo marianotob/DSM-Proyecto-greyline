d02743a3c88aedaebbde3f9e379a4c1ae4bbed10
Manu <manuelgc_12@hotmail.com>
2016-12-02 16:45:52 +0100

    Creada vista basica de ver libreria y vista detalles de libro, con us controladores

--- Entrega1Gen/MvcApplication1/Controllers/HomeController.cs
+++ Entrega1Gen/MvcApplication1/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Entrega1GenNHibernate.CEN.GrayLine;
+using Entrega1GenNHibernate.EN.GrayLine;
+using Entrega1GenNHibernate.CAD.GrayLine;
+using System.IO;
+using MvcApplication1.Models;
 
 namespace MvcApplication1.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BasicController
     {
         public ActionResult Index()
         {
@@ -28,5 +34,14 @@ namespace MvcApplication1.Controllers
 
             return View();
         }
+
+      /*  public ActionResult Destacados()
+        {
+            LibroCEN cen = new LibroCEN();
+            IEnumerable<LibroEN> list = cen.VerLibreria(0, -1).ToList();
+
+            return PartialView("_DestacadosPartial", list);
+        }
+        */
     }
 }
